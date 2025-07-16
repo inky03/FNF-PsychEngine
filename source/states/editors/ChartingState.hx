97dac95d2055c7fad67830cfef0d526a18aae128
ashley <84390581+inky03@users.noreply.github.com>
2025-07-16 06:50:26 -0600

    fix chart editor "ghost" notes

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -2218,7 +2218,8 @@ class ChartingState extends ScriptedState implements PsychUIEventHandler.PsychUI
 		var maxTime:Float = getMaxNoteTime(curSec);
 		function curSecFilter(note:MetaNote)
 		{
-			return (note.strumTime >= minTime && note.strumTime + 1.7 < maxTime);
+			var timeAdjusted:Float = (note.strumTime + 3);
+			return (timeAdjusted >= minTime && timeAdjusted < maxTime);
 		}
 
 		var firstNote:Bool = false;
@@ -2261,8 +2262,9 @@ class ChartingState extends ScriptedState implements PsychUIEventHandler.PsychUI
 				var nextMaxTime:Float = getMaxNoteTime(curSec+1);
 				function otherSecFilter(note:MetaNote)
 				{
-					return (prevGridBg.visible && (note.strumTime >= prevMinTime && note.strumTime < prevMaxTime)) ||
-						(nextGridBg.visible && (note.strumTime >= nextMinTime && note.strumTime < nextMaxTime));
+					var timeAdjusted:Float = (note.strumTime + 3);
+					return (prevGridBg.visible && (timeAdjusted >= prevMinTime && timeAdjusted < prevMaxTime)) ||
+						(nextGridBg.visible && (timeAdjusted >= nextMinTime && timeAdjusted < nextMaxTime));
 				}
 	
 				for(note in notes.filter(otherSecFilter))
@@ -2534,23 +2536,31 @@ class ChartingState extends ScriptedState implements PsychUIEventHandler.PsychUI
 
 			if(changed)
 			{
-				var textureLoad:String = 'images/${noteTextureInputText.text}.png';
-				if(Paths.fileExists(textureLoad, IMAGE) || noteTextureInputText.text.trim() == '')
-				{
-					for (note in notes)
-					{
-						if(note == null) continue;
-						note.reloadNote(note.texture);
-						
-						if(note.width > note.height)
-							note.setGraphicSize(GRID_SIZE);
-						else
-							note.setGraphicSize(0, GRID_SIZE);
-						
-						note.updateHitbox();
-						positionNoteXByData(note);
+				var textureLoad:String = noteTextureInputText.text;
+				var changedTexture:Bool = false;
+				
+				for (note in notes) {
+					if (note == null) continue;
+					
+					var oldTexture:String = note.graphic?.key;
+					note.texture = textureLoad;
+					if (note.graphic?.key == oldTexture) {
+						break;
+					} else {
+						changedTexture = true;
 					}
 					
+					if (note.width > note.height) {
+						note.setGraphicSize(GRID_SIZE);
+					} else {
+						note.setGraphicSize(0, GRID_SIZE);
+					}
+					
+					note.updateHitbox();
+					positionNoteXByData(note);
+				}
+				
+				if (changedTexture) {	
 					var startX:Float = gridBg.x;
 					var startY:Float = (FlxG.height - GRID_SIZE) / 2;
 					
