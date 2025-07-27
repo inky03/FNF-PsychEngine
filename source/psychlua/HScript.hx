9a9219242e57a9969998d0bcbbe56391dab7a980
ashley <84390581+inky03@users.noreply.github.com>
2025-07-26 19:34:30 -0600

    Update HScript.hx
    Auto import current state (probably)

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -263,6 +263,9 @@ class HScript extends Iris {
 		set('FlxAnimate', FlxAnimate);
 		#end
 		set('controls', Controls.instance);
+		
+		if (parentState != null && !(parentState is ScriptedSubState))
+			set(ScriptedSubState.getStateName(parentState), Type.getClass(parentState));
 
 		// Functions & Variables
 		var variableMap:Map<String, Dynamic> = getVariables();
