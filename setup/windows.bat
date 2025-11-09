360d6d27a59b178c9458d1edbcc594d8792e69e7
ashley <84390581+inky03@users.noreply.github.com>
2025-11-09 01:44:54 -0600

    funkin.vis commit in lib setup

--- setup/windows.bat
+++ setup/windows.bat
@@ -15,7 +15,7 @@ haxelib install hxvlc --skip-dependencies
 haxelib git hscript-iris https://github.com/pisayesiwsi/hscript-iris.git dev
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate.git dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
-haxelib git funkin.vis https://github.com/FunkinCrew/funkVis.git
+haxelib git funkin.vis https://github.com/FunkinCrew/funkVis.git 1966f8fbbbc509ed90d4b520f3c49c084fc92fd6
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git
 echo Finished!
 pause
